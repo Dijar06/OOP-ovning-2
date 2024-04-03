b1f6fa602f40d3bcde9ad678c65eb07411b95083
Dijar06 <dize06001@utb.vaxjo.se>
2024-04-03 11:19:03 +0200

    Add project files.

---
+++ OOP övning 2/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_övning_2
+{
+    class Class1
+    {
+        private string förnamn;
+        private string efternamn;
+
+        public string metod1
+        {
+            get { return förnamn; }
+            set { förnamn = value; }
+        }
+
+        public string metod2
+        {
+            get { return efternamn; }
+            set { efternamn = value; }
+        }
+    }
+}
---
+++ OOP övning 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OOP_övning_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 p1 = new Class1();
+            p1.metod1 = "Dijar";
+            p1.metod2 = "Zejnullahi";
+
+            Console.WriteLine(p1.metod1 + " " + p1.metod2);
+        }
+    }
+}
